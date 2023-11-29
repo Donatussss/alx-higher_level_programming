1e393f83677ad53d9a3f0f3580853b07daaf4ee7
Donatussss <don.sp4r@gmail.com>
2023-11-29 05:42:35 +0300

    0x01 13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,67 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_temp - a function that inserts a number
+ * into a singly linked list
+ * @head: head of the singly linked list
+ * @number: number to be added
+ * Return: the address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_temp(listint_t *head, int number)
+{
+	listint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	new->next = head->next;
+	head->next = new;
+
+	return (new);
+}
+
+/**
+ * insert_node_temp - a function that inserts a number into a
+ * sorted singly linked list
+ * @head: head of the singly linked list
+ * @number: new number to be added
+ * Return: the address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_node_temp(listint_t *head, int number)
+{
+	if (head == NULL)
+		return (NULL);
+
+	if (head->next != NULL)
+	{
+		if (number > (head->next)->n)
+			return(insert_node_temp(head->next, number));
+	}
+	return (insert_temp(head, number));
+
+}
+
+
+/**
+ * insert_node - a function that inserts a number into a
+ * sorted singly linked list
+ * @head: head of the singly linked list
+ * @number: new number to be added
+ * Return: the address of the new node, of NULL if it failed
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	if (head == NULL)
+		return (NULL);
+	if (number > (*head)->n)
+		return (insert_node_temp(*head, number));
+	else
+		return (insert_temp(*head, number));
+}
