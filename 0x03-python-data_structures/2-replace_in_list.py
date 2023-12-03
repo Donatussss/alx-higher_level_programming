391047483672fc21ab7a59168a48de90810e064b
Donatussss <don.sp4r@gmail.com>
2023-12-04 01:35:23 +0300

    0x03 2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+replace_in_list - a function that replaces an element
+of a list at a specific position (like in C)
+If idx is negative, the function should not modify anything,
+and returns the original list
+If idx is out of range (> of number of element in my_list),
+the function should not modify anything, and returns the
+original list
+original list
+You are not allowed to import any module
+You are not allowed to use try/except
+"""
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    temp = my_list[:]
+    temp[idx] = element
+    return temp
