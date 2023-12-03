d9c9ba19b5a7405bda028c1365d516b15d6a3a54
Donatussss <don.sp4r@gmail.com>
2023-12-04 02:11:29 +0300

    0x03 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        i_len = len(i)
+        for idj, j in enumerate(i):
+            post_fix = '' if idj == i_len - 1 else ' '
+            end_format = '' if post_fix == ' ' else '\n'
+            print('{:d}{}{}'.format(j, post_fix, end_format), end='')
