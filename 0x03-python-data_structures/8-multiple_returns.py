81823cad6a3ab9d0df933f30fb5ec7284acfa7c6
Donatussss <don.sp4r@gmail.com>
2023-12-04 02:34:14 +0300

    0x03 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- multiple_returns - a function that returns a 
+ multiple_returns - a function that returns a
  tuple with the length of a string and its first character
  If the sentence is empty, the first character should be equal to None
  You are not allowed to import any module
