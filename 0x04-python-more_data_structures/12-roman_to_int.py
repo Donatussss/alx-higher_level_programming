bfbe69de9fca4b3f9459f4d34560035d17a2c553
Donatussss <don.sp4r@gmail.com>
2023-12-07 04:27:18 +0300

    0x04 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,15 @@
 def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) is not str:
         return None
-    num_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
     rev = roman_string[::-1]
     num = None
     for id_c, c in enumerate(rev):
