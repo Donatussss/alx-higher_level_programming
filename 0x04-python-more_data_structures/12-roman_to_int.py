ce045f6fbcea1eb4df7bd1d69648b844a8c636a1
Donatussss <don.sp4r@gmail.com>
2023-12-07 04:33:48 +0300

    0x04 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -14,6 +14,8 @@ def roman_to_int(roman_string):
     rev = roman_string[::-1]
     num = None
     for id_c, c in enumerate(rev):
+        if c is not in num_dict:
+            return 0
         if num is None:
             num = num_dict[c]
             continue
