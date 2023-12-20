d3d2745550e250f556907fc2697c3cdba54e145c
Donatussss <don.sp4r@gmail.com>
2023-12-20 14:11:54 +0530

    0x06 6

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -28,8 +28,8 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Private instance attribute size instantiated with size"""
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
