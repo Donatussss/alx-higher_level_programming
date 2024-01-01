af95a9e058083f917d9ef84c37794861c5eb7c8c
Donatussss <don.sp4r@gmail.com>
2024-01-01 21:18:22 +0300

    taking a break

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-for ch_r in reversed(range(97, 123)):
-    print("{:c}".format(ch_r if (ch_r % 2 == 0) else (ch_r - 32)), end='')
--- 0x01-python-if_else_loops_functions/7-islower.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def islower(x):
-    return ord(x) in range(97, 123)
